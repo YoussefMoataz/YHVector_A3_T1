efcad2fe8d98a9dbda415f32d738c22bde95f768
Youssef Moataz <yousefcrunshy03@gmail.com>
2022-12-09 15:11:54 +0200

    Added comments for main function.

--- YHVectorApp.cpp
+++ YHVectorApp.cpp
@@ -11,8 +11,8 @@ int main() {
 
     try {
 
-        cout << v1[2] << endl;
-        cout << v1[5] << endl;
+        cout << v1[2] << endl; // 35
+        cout << v1[5] << endl; // exception
 
     } catch (out_of_range &e) {
 
@@ -20,18 +20,14 @@ int main() {
 
     }
 
-//    for (int i = 0; i < vv3.size(); ++i) {
-//        cout<<vv3[i]<<endl;
-//    }
-
-    cout << *(v1.begin()) << endl;
-    cout << *(v1.end() - 1) << endl;
-    cout << (v1.begin()) << endl;
-    cout << (v1.end() - 1) << endl;
+    cout << *(v1.begin()) << endl; // 123
+    cout << *(v1.end() - 1) << endl; // 5445
+    cout << (v1.begin()) << endl; // address
+    cout << (v1.end() - 1) << endl; // address
 
     printDashes();
 
-    cout << v1 << endl;
+    cout << v1 << endl; // the vector
 
     printDashes();
 
@@ -41,22 +37,22 @@ int main() {
 
     sort(v2.begin(), v2.end());
 
-    cout << v2 << endl;
+    cout << v2 << endl; // the sorted vector
 
     printDashes();
 
     int a3[5] = {123, 22, 35, 41, 5445};
     YHVector<int> v3(a3, 5);
 
-    cout << v3.size() << endl;
-    cout << v3.capacity() << endl;
-    cout << v3 << endl;
+    cout << v3.size() << endl; // 5
+    cout << v3.capacity() << endl; // 5
+    cout << v3 << endl; // the vector
 
     v3.push_back(642);
 
-    cout << v3.size() << endl;
-    cout << v3.capacity() << endl;
-    cout << v3 << endl;
+    cout << v3.size() << endl; // 6
+    cout << v3.capacity() << endl; // 10
+    cout << v3 << endl; // the vector
 
     cout << v3.empty() << endl; // 0
 
@@ -69,44 +65,44 @@ int main() {
     string a4[4] = {"first", "second", "third", "fourth"};
     YHVector<string> v4(a4, 4);
 
-    cout << v4.size() << endl;
-    cout << v4.capacity() << endl;
-    cout << v4 << endl;
+    cout << v4.size() << endl; // 4
+    cout << v4.capacity() << endl; // 4
+    cout << v4 << endl; // the vector
 
     v4.push_back("finall");
 
-    cout << v4.size() << endl;
-    cout << v4.capacity() << endl;
-    cout << v4 << endl;
+    cout << v4.size() << endl; // 5
+    cout << v4.capacity() << endl; // 8
+    cout << v4 << endl; // the vector
 
     v4.push_back("last one");
     v4.push_back("last two");
 
-    cout << v4.size() << endl;
-    cout << v4.capacity() << endl;
-    cout << v4 << endl;
+    cout << v4.size() << endl; // 7
+    cout << v4.capacity() << endl; // 8
+    cout << v4 << endl; // the vector
 
     v4.clear();
 
-    cout << v4.size() << endl;
-    cout << v4.capacity() << endl;
-    cout << v4 << endl;
+    cout << v4.size() << endl; // 0
+    cout << v4.capacity() << endl; // 8
+    cout << v4 << endl; // none
 
     printDashes();
 
     string a5[4] = {"first", "second", "third", "fourth"};
     YHVector<string> v5(a5, 4);
 
-    cout << v5 << endl;
-    cout << v5.size() << endl;
+    cout << v5 << endl; // the vector
+    cout << v5.size() << endl; // 4
 
     v5.pop_back();
     v5.pop_back();
     v5.pop_back();
     v5.pop_back();
 
-    cout << v5 << endl;
-    cout << v5.size() << endl;
+    cout << v5 << endl; // none
+    cout << v5.size() << endl; // 0
 
     printDashes();
 
@@ -115,10 +111,10 @@ int main() {
 
     *v6.begin() = *(v6.begin() + 3);
 
-    cout << *(v6.begin()) << endl;
-    cout << *(v6.end() - 1) << endl;
-    cout << (v6.begin()) << endl;
-    cout << (v6.end() - 1) << endl;
+    cout << *(v6.begin()) << endl; // 41
+    cout << *(v6.end() - 1) << endl; // 5445
+    cout << (v6.begin()) << endl; // address
+    cout << (v6.end() - 1) << endl; // address
 
     printDashes();
 
@@ -126,9 +122,9 @@ int main() {
     YHVector<int> v7(a7, 5);
     YHVector<int> v8(v7);
 
-    cout << v8 << endl;
-    cout << v8.size() << endl;
-    cout << v8.capacity() << endl;
+    cout << v8 << endl; // the vector
+    cout << v8.size() << endl; // 5
+    cout << v8.capacity() << endl; // 5
 
     printDashes();
 
@@ -136,14 +132,14 @@ int main() {
     YHVector<int> v9(a9, 5);
     YHVector<int> v10 = v9;
 
-    cout << v10 << endl;
+    cout << v10 << endl; // the vector
 
     v10.pop_back();
     v10.pop_back();
 
-    cout << v10 << endl;
-    cout << v10.size() << endl;
-    cout << v10.capacity() << endl;
+    cout << v10 << endl; // the vector
+    cout << v10.size() << endl; // 3
+    cout << v10.capacity() << endl; // 5
 
     printDashes();
 
@@ -157,58 +153,56 @@ int main() {
 
     }
 
-    cout << v12 << endl;
-    cout << v12.size() << endl;
+    cout << v12 << endl; // the vector
+    cout << v12.size() << endl; // 5
 
     printDashes();
 
     string a13[5] = {"first", "second", "third", "fourth", "fifth"};
     YHVector<string> v13(a13, 5);
 
-    cout << v13.begin() << endl;
-    cout << v13.end() << endl;
+    cout << v13.begin() << endl; // address
+    cout << v13.end() << endl; // address
 
-
-    cout << v13 << endl;
+    cout << v13 << endl; // the vector
 
     v13.erase(v13.begin() + 3);
 
-    cout << v13 << endl;
+    cout << v13 << endl; // the vector
 
     printDashes();
 
     string a14[8] = {"zero", "first", "second", "third", "fourth", "fifth", "sixth", "seventh"};
     YHVector<string> v14(a14, 8);
 
-    cout << v14.begin() << endl;
-    cout << v14.end() << endl;
+    cout << v14.begin() << endl; // address
+    cout << v14.end() << endl; // address
 
-
-    cout << v14 << endl;
+    cout << v14 << endl; // the vector
 
     v14.erase(v14.begin() + 2, v14.begin() + 5);
 
-    cout << v14 << endl;
+    cout << v14 << endl; // the vector
 
     printDashes();
 
     string a15[5] = {"first", "second", "third", "fourth", "fifth"};
     YHVector<string> v15(a15, 5);
-    cout << v15.begin() << endl;
-    cout << v15.end() << endl;
 
+    cout << v15.begin() << endl; // address
+    cout << v15.end() << endl; // address
 
-    cout << v15 << endl;
-    cout << v15.size() << endl;
+    cout << v15 << endl; // the vector
+    cout << v15.size() << endl; // 5
 
     v15.insert(v15.begin() + 4, "The new item");
 
-    cout << v15 << endl;
-    cout << v15.size() << endl;
+    cout << v15 << endl; // the vector
+    cout << v15.size() << endl; // 6
 
     printDashes();
 
-    //test for == operator and < operator
+    // test for == operator and < operator
     int a16[5] = {1, 1, 3, 4, 5};
     YHVector<int> v16(a16, 5);
     int a17[5] = {1, 2, 3, 4, 5};
